6636410e10947d5b41d0a330bfad5878fee2c964
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-01-04 14:41:19 -0400

    Add Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,18 @@
-const Header = () => <div>Header</div>;
+import GoBackIcon from '../assets/images/GoBackIcon.png';
+import MicIcon from '../assets/images/MicIcon.png';
+import SettingsIcon from '../assets/images/SettingsIcon.png';
+
+const Header = () => (
+  <header>
+    <nav>
+      <button type="button" className="navigation-icons"><img src={GoBackIcon} alt="Go back icon" /></button>
+      <h1>I Love Food</h1>
+      <div>
+        <button type="button" className="navigation-icons"><img src={MicIcon} alt="Go back icon" /></button>
+        <button type="button" className="navigation-icons"><img src={SettingsIcon} alt="Go back icon" /></button>
+      </div>
+    </nav>
+  </header>
+);
 
 export default Header;
